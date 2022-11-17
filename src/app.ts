f18a0bd7bb405db71c1e59922af685982d28fb43
kaduh15 <kadu.siva2014@gmail.com>
2022-11-17 14:54:11 -0300

    Fix lint

--- src/app.ts
+++ src/app.ts
@@ -16,10 +16,13 @@ class App {
     this.app.get('/', (req, res) => res.json({ ok: true }));
   }
 
-  private config():void {
+  private config(): void {
     const accessControl: express.RequestHandler = (_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET,POST,DELETE,OPTIONS,PUT,PATCH',
+      );
       res.header('Access-Control-Allow-Headers', '*');
       next();
     };
@@ -28,13 +31,12 @@ class App {
     this.app.use(accessControl);
   }
 
-  private routes():void {
-    
+  private routes(): void {
     // Rota de Erro!
     this.app.use(errorMiddleware);
   }
 
-  public start(PORT: string | number):void {
+  public start(PORT: string | number): void {
     this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
   }
 }

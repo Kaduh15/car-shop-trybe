af42538d648695a242ac54561060db8a384e0b02
kaduh15 <kadu.siva2014@gmail.com>
2022-11-17 14:48:09 -0300

    Start API

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,45 @@
 import express from 'express';
+import errorMiddleware from './middlewares/ErrorMiddleware';
 
-const app = express();
+// import 'express-async-errors';
 
-export default app;
+class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+
+    this.config();
+    this.routes();
+
+    // Não remover essa rota
+    this.app.get('/', (req, res) => res.json({ ok: true }));
+  }
+
+  private config():void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(express.json());
+    this.app.use(accessControl);
+  }
+
+  private routes():void {
+    
+    // Rota de Erro!
+    this.app.use(errorMiddleware);
+  }
+
+  public start(PORT: string | number):void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+}
+
+export default App;
+
+// A execução dos testes de cobertura depende dessa exportação
+export const { app } = new App();
